05353c8f6daa3a3e735ce11d95e07908918ac9d8
JAM RYAN <a0979091221@gmail.com>
2018-05-31 21:42:50 +0800

    Add and modify table and UI

--- app/event/status_channel.js
+++ app/event/status_channel.js
@@ -7,13 +7,37 @@ const { remote, ipcRenderer } = require('electron');
 const currentWindow = remote.getCurrentWindow();
 const path = require('path');
 const url = require('url');
+const $  = require( 'jquery' );
+const dt = require( 'datatables.net' )();
 
 const utils = require('../utils/ui')
 
 // send message to update table
 ipcRenderer.send('update_bot_status',{})
 
-
+// DataTable variable
+var botStatusTable
+// DataTable initialization
+botStatusTable = $('#bot_status_table').DataTable( {
+    "createdRow": function ( row, data, index ) {
+                $('td', row).eq(0).addClass('normal');
+                $('td', row).eq(1).addClass('market');
+            },
+        "autoWidth": true,
+        "paging": true,
+        columnDefs: [{ 
+            "orderable": false, 
+            "targets": 2 
+        }],
+        fixedColumns: true,
+        autoFill: true,
+        data: [],
+        columns: [
+            { title: "BOT ID" },
+            { title: "DETAILS" },
+            { title: "MANAGEMENT" }
+        ]
+} );
 /**
  * Create bot instance event
  */
@@ -46,36 +70,63 @@ ipcRenderer.on('receive_bot_status',(event,arg)=>{
      */
 
     // drop
-	let list = document.getElementById("bot_status_table")
-	while (list.firstChild) {
-		list.removeChild(list.firstChild);
-    }
-    
+	// let list = document.getElementById("bot_status_table")
+	// while (list.firstChild) {
+	// 	list.removeChild(list.firstChild);
+ //    }
+    // Clean Bot Table before every update
+    botStatusTable.clear()
+
     for(let i in arg.id_queue){
-        let tr = document.createElement("TR");
+        // let tr = document.createElement("TR");
 
-        let td=document.createElement("TD")
-        td.innerHTML = arg.id_queue[i]
-        tr.appendChild(td)
-        let detail=document.createElement("TD")
-        detail.innerHTML = "[WIP]"
-        tr.appendChild(detail)
-        let btn_td=document.createElement("TD")
-        let btn = document.createElement("BUTTON")
-        btn.setAttribute("class","btn btn-danger")
-        btn.innerHTML = "Kill Bot"
-        // btn.setAttribute("onclick","call_kill(\""+arg.id_queue[i]+"\")");
-        btn.addEventListener("click",function(event){
-            console.log("Killing ... id=",arg.id_queue[i])
-            // send the killing signal to event.js
-            ipcRenderer.send('kill_bot',{
-                id: arg.id_queue[i]
-            })
-        })
-        btn_td.appendChild(btn)
-        tr.appendChild(btn_td)
-
+        // let td=document.createElement("TD")
+        // td.innerHTML = arg.id_queue[i]
+        // tr.appendChild(td)
+        // let detail=document.createElement("TD")
+        // detail.innerHTML = "[WIP]"
+        // tr.appendChild(detail)
+        // let btn_td=document.createElement("TD")
+        // let btn = document.createElement("BUTTON")
+        // btn.setAttribute("class","btn btn-danger")
+        // btn.innerHTML = "Kill Bot"
+        // // btn.setAttribute("onclick","call_kill(\""+arg.id_queue[i]+"\")");
+        // btn.addEventListener("click",function(event){
+        //     console.log("Killing ... id=",arg.id_queue[i])
+        //     // send the killing signal to event.js
+        //     ipcRenderer.send('kill_bot',{
+        //         id: arg.id_queue[i]
+        //     })
+        // })
+        // btn_td.appendChild(btn)
+        // tr.appendChild(btn_td)
+        //Delete buttons
+        let d_btn = '<button type="button" class="btn btn-danger btn-sm dt-delete"><i class="fas fa-minus-circle" style="font-size: 20px;"></i></button>'
+        let e_btn = '<button type="button" class="btn btn-primary btn-sm dt-edit"><i class="fas fa-sliders-h" style="font-size: 20px;"></i></button>'
+        let mn_btn = e_btn+"&nbsp;"+d_btn
+        let dt_arr = [
+            arg.id_queue[i],
+            "[WIP]",
+            mn_btn
+        ]
         // append into target
-        document.getElementById("bot_status_table").appendChild(tr)
+        // document.getElementById("bot_status_table").appendChild(tr)
+        botStatusTable.row.add(dt_arr).draw();
     }
+    //Delete buttons event (Only works here....)
+    $('.dt-delete').each(function () {
+        $(this).on('click', function(evt){
+            $this = $(this);
+            var data = botStatusTable.row( $(this).parents('tr') ).data();
+            if(confirm("Are you sure to delete this bot?")){
+                botStatusTable.row( $(this).parents('tr') ).remove().draw();
+                console.log("Killing ... id=",data[0])
+                // send the killing signal to event.js
+                ipcRenderer.send('kill_bot',{
+                    id: data[0]
+                })
+            }
+        });
+    });
+
 })
