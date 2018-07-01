beb01d09a0456f7a259afa82e2270f0b1133040b
kevinbird61 <kevinbird61@gmail.com>
2018-07-01 10:38:42 +0800

    Upgrade trade bot

--- src/model/trade_backtrack.js
+++ src/model/trade_backtrack.js
@@ -4,12 +4,13 @@
  * - 用以判斷買賣、以獲得最大獲利
  */
 const request = require('request');
+const rp = require('request-promise')
 const YAML = require("yamljs");
 const trade_backtracking = {};
 
 // url definition
 const backtrack_url = "https://ectrader-backtesting.herokuapp.com/"
-
+const timeout = 10*60*1000
 /**
  * 
  * @param yaml_string       讀取交易策略後，回傳的檔案內容（string）
@@ -32,6 +33,32 @@ trade_backtracking.backtrack = function(yaml_string,start,end){
                 console.log(body)
             }
         })
+}
+
+trade_backtracking.backtrackPromise = function(yaml_string, start_time, end_time){
+    let data = {
+        yaml_string: yaml_string,
+        start: start_time,
+        end: end_time
+    }
+
+    let options = {
+        method: 'POST',
+        url: backtrack_url,
+        form: data,
+        timeout: timeout,
+        json: true
+    }
+
+    return new Promise((resolve, reject) => {
+        rp(options)
+        .then(function(parsedBody){
+            resolve(parsedBody)
+        })
+        .catch(function(err){
+            reject(err)
+        })
+    })
 }
 
 module.exports = trade_backtracking;
