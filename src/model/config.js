9a5f40465ef6524b1f10983b47fae447d5384f55
kevinbird61 <kevinbird61@gmail.com>
2018-07-04 12:49:02 +0800

    Update api usage.

--- src/model/config.js
+++ src/model/config.js
@@ -6,6 +6,7 @@
  */
 // configuration
 const config = require("../config/config.default");
+const {db} = require('../model/db');
 
 class reconfig{
     constructor(){
@@ -22,6 +23,21 @@ class reconfig{
         config.set_server(arg.server);
         // set trade 
         config.set_trade(arg.trade);
+        // update 
+        db.store_api_ks(config.username,arg.trade.binance_apiKey,arg.trade.binance_apiSecret,
+            (err,msg)=>{
+                if(err){
+                    console.error("Error when api config...");
+                    console.log(err);
+                }
+                else{
+                    console.log("Store your new binance API info!")
+                    // and then write back
+                    config.record_trade();
+                    // send success signal
+                    event.sender.send("api_config_success",{})
+                }
+            })
     }
 
     reset(){
