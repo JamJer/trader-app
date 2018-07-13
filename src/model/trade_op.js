dd4b3591963ba54bc3dad373bfc544333b0d5363
kevinbird61 <kevinbird61@gmail.com>
2018-07-14 02:04:06 +0800

    Fix binance client instance config.

--- src/model/trade_op.js
+++ src/model/trade_op.js
@@ -98,6 +98,7 @@ class trade_op {
 
             let result = [];
             // fetch the target data
+            this.client = Binance({apiKey: this.binance_apikey, apiSecret: this.binance_apisecret})
             let data = await this.client.candles({ symbol: symbol, interval: interval})
             await data.map((day, index) => {
                 let ary = {}
@@ -127,6 +128,7 @@ class trade_op {
     async price(symbol){
         console.log("Request Price ...")
         try{
+            this.client = Binance({apiKey: this.binance_apikey, apiSecret: this.binance_apisecret})
             let price = await this.client.prices();
             return parseFloat(price[symbol]);
         }
@@ -149,6 +151,7 @@ class trade_op {
             let interval = '5m'
             let tempVA=[]
             // fetch the data
+            this.client = Binance({apiKey: this.binance_apikey, apiSecret: this.binance_apisecret})
             let data = await this.client.candles({symbol: symbol, interval:interval})
             await data.map((day,index) => {
                 let ary = {};
