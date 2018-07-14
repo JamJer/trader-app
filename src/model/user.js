6d886bbfd34c103a7dfde3eccc6d83e89905d1bb
kevinbird61 <kevinbird61@gmail.com>
2018-07-14 10:04:42 +0800

    Add product key check.

--- src/model/user.js
+++ src/model/user.js
@@ -21,27 +21,42 @@ class user{
             // redirect to new link
             console.log(res.statusCode)
             console.log(res.headers['location'])
+            let dbURL = res.headers['location'];
             rp.post(res.headers['location'],{form: arg})
                 .then((body)=>{
                     // Body will be the result
                     let res = JSON.parse(body);
-                    //
                     if(res.msg=="success"){
                         // Store in session 
                         config.store_user(arg.username)
-                        // Store
-                        db.store_product_key(arg.username,arg.passwd,res.key,
-                            (err,msg)=>{
-                                if(err)
-                                    console.log(err);
-                                // Send back
-                                // if res.msg == OK, then represent this user is legal
-                                // FIXME: In debug mode, all msg will return OK, without compare user data
-                                // And when this user login success, it will get a unique key of this user to activate trade bot
-                                event.sender.send('login-success',res.key);
-                                
-                                // trader.prepare(arg.username);
-                            });
+                        // examine product key
+                        console.log("Now starting the product key's examinate.")
+                        let product_key = res.key;
+                        rp.post(dbURL,{form: {username: arg.username, passwd: arg.passwd, key_id: res.key}})
+                            .then((body) => {
+                                let response = JSON.parse(body);
+                                if(response.msg=="success"){
+                                    // product key are activated
+                                    // Store product key 
+                                    console.log("Product key examinate success, store all user's config.")
+                                    db.store_product_key(arg.username,arg.passwd,product_key,
+                                        (err,msg)=>{
+                                            if(err)
+                                                console.log(err);
+                                            // Send back
+                                            // if res.msg == OK, then represent this user is legal
+                                            // FIXME: In debug mode, all msg will return OK, without compare user data
+                                            // And when this user login success, it will get a unique key of this user to activate trade bot
+                                            event.sender.send('login-success',product_key);
+                                            
+                                            // trader.prepare(arg.username);
+                                        });
+                                }
+                                else{
+                                    // send error msg
+                                    event.sender.send('login-error', response.msg)
+                                }
+                            })
                     }
                     else{
                         event.sender.send('login-error',res.msg);
